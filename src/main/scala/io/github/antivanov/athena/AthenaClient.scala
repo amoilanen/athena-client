971a888bf6eca135cb094648e81ec9f600b271ef
Anton Ivanov <anton.al.ivanov@gmail.com>
2019-08-19 23:01:10 +0300

    Add some initial test data and database creation script

---
+++ src/main/scala/io/github/antivanov/athena/AthenaClient.scala
@@ -0,0 +1,5 @@
+package io.github.antivanov.athena
+
+class AthenaClient {
+
+}
---
+++ src/test/scala/io/github/antivanov/athena/AthenaClientSpec.scala
@@ -0,0 +1,7 @@
+package io.github.antivanov.athena
+
+import org.scalatest.{FreeSpec, Matchers}
+
+class AthenaClientSpec extends FreeSpec with Matchers {
+
+}
